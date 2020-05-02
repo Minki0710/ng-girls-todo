4b24f26d77def0c06626bdb88f47841b15ae2352
Minki0710 <ankita.upadhyay.7@outlook.com>
2020-05-02 14:01:12 +0530

    Interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
   title = 'todo-list';
+  todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }
