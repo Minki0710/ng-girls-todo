c5a3ab1079c19a2d00699fede3f3b4e6fe8cc3fc
Minki0710 <ankita.upadhyay.7@outlook.com>
2020-05-02 14:04:43 +0530

    Using Interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TodoItem } from '../interfaces/todo-item';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'my-app',
