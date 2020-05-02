9593241886677a7682fd70bcedec6bf71893098d
Minki0710 <ankita.upadhyay.7@outlook.com>
2020-05-02 14:10:30 +0530

    EventEmitter

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
   template: `
-     <input #inputElementRef
-         [value]="title"
-         (keyup.enter)="changeTitle(inputElementRef)">
+    <input #inputElementRef
+           [value]="title"
+           (keyup.enter)="submitValue($event.target.value)">
 
-  <button (click)="changeTitle(inputElementRef)">
-    Save
-  </button>
-  `,  
-  styleUrls: ['./input-button-unit.component.css']  
-})    
+    <button (click)="submitValue(inputElementRef.value)">
+      Save
+    </button>
+  `,
+  styleUrls: ['./input-button-unit.component.css']
+})
 export class InputButtonUnitComponent implements OnInit {
+
+  @Output() submit: EventEmitter<string> = new EventEmitter();
+
   title = 'Hello World';
 
-  constructor() { }                     
+  constructor() { }
 
   ngOnInit() {
   }
 
- changeTitle(inputElementReference) {
-  inputElementReference.value= "Hello";
-  this.title = inputElementReference.value;
-}
+  submitValue(newTitle: string) {
+    this.submit.emit(newTitle);
+  }
 }
